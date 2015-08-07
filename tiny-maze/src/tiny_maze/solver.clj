d96fe05d126a0f18845dea57fe3000c54ec2fbe0
Cole Willsea <coleww@gmail.com>
2015-08-07 13:26:58 -0700

    WOW no errors!

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,119 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(defn findit [maze targ]
+  (let [the-maze (flatten maze)]
+    (nth the-maze (->
+                   (map #(:content %) the-maze)
+                   (.indexOf targ)))))
+
+(defn map-cell
+  [cell x y]
+  {:x x
+   :y y
+   :content cell
+   :g 0
+   :h 0
+   :f 0
+   :parent nil})
+
+(defn map-row
+  [row y]
+  (map-indexed #(map-cell %2 %1 y) row))
+
+(defn map-maze
+  [maze]
+  (map-indexed #(map-row %2 %1) maze))
+
+(defn get-neighbours
+  [cell maze]
+  (let [x (:x cell)
+        y (:y cell)
+        x-max (count (first maze))
+        y-max (count maze)
+        diffs [[1 1] [0 1] [0 -1] [1 0] [-1 0] [1 -1] [-1 1] [-1 -1]]
+        offs (map #(vector (+ x (first %)) (+ y (second %))) diffs)
+        in-offs (filter #(and (< -1 (second %) y-max) (< -1 (first %) x-max)) offs)]
+    (map #(nth (nth maze (second %)) (first %)) in-offs)))
+
+(defn get-g-cost [parent target]
+  ; if its diagonal, 14, horiz/vert, 10
+  (+ (:g parent) (if (= 1 (Math/abs (+ (- (:y parent) (:y target))  (- (:x parent) (:x target)))))
+          10
+          14)))
+
+(defn get-h-cost [current exit]
+  (* 10 (+ (Math/abs (- (:x current) (:x exit))) (Math/abs (- (:y current) (:y exit))))))
+
+(defn is-in-list [list item]
+  (some #(and (= (:x item) (:x %)) (= (:y item) (:y %))) list))
+
+(defn solve-maze [maze]
+  (let [the-maze (map-maze maze)
+        start (findit the-maze :S)
+        end (findit the-maze :E)
+        open-list (atom [start])
+        closed-list (atom [])
+        found-it (atom false)]
+    ; until we find it, or run out of elements in the open-list
+    (while (and (not @found-it) (pos? (count @open-list)))
+      (do
+        ; sort the open-list by f score
+        (swap! open-list #(sort-by :f %))
+
+        (let [current (first @open-list)
+              neighbours (get-neighbours current the-maze)]
+          ;; shift off first element of the open list
+          (swap! open-list rest)
+          ;; add it to the closed list
+          (swap! closed-list conj current)
+
+          (doseq [el neighbours]
+            ; if this el is not in the closed-list, and it is not a wall
+            (if (or (not (is-in-list @closed-list el)) (not= 1  (:content el)))
+              (if (is-in-list @open-list el)
+                 ; if it is in the open list
+                (do
+                  (if (< (get-g-cost current el) (:g el))
+                    (let [g (get-g-cost current el)
+                        h (:h el)
+                        f (+ g h)
+                        updated-el (->
+                                    el
+                                    (assoc :parent current)
+                                    (assoc :f f)
+                                    (assoc :g g))
+                          new-list (filter #(or  (not=  (:x %) (:x updated-el)) (not= (:y %) (:y updated-el))  ) @open-list)]
+
+                      (reset! open-list (cons updated-el new-list)))))
+                (do
+                  ; g cost is 10 for horiz/vert, 14 for diag  movement from parent + the g cost of parent
+                  (let [g (get-g-cost current el)
+                        h (get-h-cost el end)
+                        f (+ g h)
+                        updated-el (->
+                                    el
+                                    (assoc :parent current)
+                                    (assoc :f f)
+                                    (assoc :g g)
+                                    (assoc :h h)
+                                    )]
+                    (if (= (:content el) :E) (reset! found-it el) )
+                    (swap! open-list conj updated-el)))))))))
+
+
+    ;; this is the end square
+    ;@found-it
+    ;; follow its parentage backward
+    ;; WAIT are the pointers even correct? fuck
+    ;; RECONSTRUCT THE PATH AND RETURN! VICTORY!
+    []
+    ))
+
+(def mazy
+  [[:S 0 0 1]
+                [1  1 0 0]
+                [1  0  0 1]
+                [1  1  0 :E]])
+
+;[(+ 1 1)]
+;(vector 0 1)
--- tiny-maze/test/tiny_maze/solver_test.clj
+++ tiny-maze/test/tiny_maze/solver_test.clj
@@ -22,3 +22,38 @@
                 [1  0 :x 1]
                 [1  1  :x :x]]]
      (is (= sol (solve-maze maze))))))
+
+(deftest test-map-cell
+  (testing "mapping a single cell"
+    (is (= (map-cell :S 5 7) {:x 5 :y 7 :content :S :g 0 :h 0 :f 0 :parent nil}))))
+
+(deftest test-map-row
+  (testing "mapping a row"
+    (is (= (map-row ["D" "O" "A"] 1) '({:x 0 :y 1 :content "D" :g 0 :h 0 :f 0 :parent nil} {:x 1 :y 1 :content "O" :g 0 :h 0 :f 0 :parent nil} {:x 2 :y 1 :content "A" :g 0 :h 0 :f 0 :parent nil})))))
+
+(deftest test-map-maze
+  (testing "mapping a row"
+    (is (= (map-maze [["D" "O" "A"] [1 2 3]]) '(({:x 0 :y 0 :content "D" :g 0 :h 0 :f 0 :parent nil} {:x 1 :y 0 :content "O" :g 0 :h 0 :f 0 :parent nil} {:x 2 :y 0 :content "A" :g 0 :h 0 :f 0 :parent nil})({:x 0 :y 1 :content 1 :g 0 :h 0 :f 0 :parent nil} {:x 1 :y 1 :content 2 :g 0 :h 0 :f 0 :parent nil} {:x 2 :y 1 :content 3 :g 0 :h 0 :f 0 :parent nil}))))))
+
+(deftest test-get-neighbours
+  (testing "getting neighbours"
+    (is (= (get-neighbours {:x 1 :y 1} (map-maze [["D" "O" "A"] [1 2 3] [4 5 6]]))    '({:x 2, :y 2, :content 6, :g 0, :h 0, :f 0, :parent nil} {:x 1, :y 2, :content 5, :g 0, :h 0, :f 0, :parent nil} {:x 1, :y 0, :content "O", :g 0, :h 0, :f 0, :parent nil} {:x 2, :y 1, :content 3, :g 0, :h 0, :f 0, :parent nil} {:x 0, :y 1, :content 1, :g 0, :h 0, :f 0, :parent nil} {:x 2, :y 0, :content "A", :g 0, :h 0, :f 0, :parent nil} {:x 0, :y 2, :content 4, :g 0, :h 0, :f 0, :parent nil} {:x 0, :y 0, :content "D", :g 0, :h 0, :f 0, :parent nil})))))
+
+
+(deftest test-get-g-cost
+  (testing "getting Gs"
+    (is (= (get-g-cost {:x 0 :y 0 :g 100} {:x 1 :y 1})  114))
+    (is (= (get-g-cost {:x 0 :y 0 :g 100} {:x 0 :y 1})  110))))
+
+(deftest test-get-h-cost
+  (testing "getting Hs"
+    (is (= (get-h-cost {:x 0 :y 0} {:x 10 :y 10}) 200))))
+
+(deftest test-is-in-list
+  (testing "checking if item is in a list"
+    (is (= (is-in-list [{:x 1 :y 1}] {:x 1 :y 1}) true))
+    (is (= (is-in-list [{:x 1 :y 0}] {:x 1 :y 1}) nil))))
+
+(deftest test-findit
+  (testing "finding a thing in the stuff"
+    (is (= (findit (map-maze [["D" "O" "A"] [1 2 3]]) 1) {:x 0 :y 1 :content 1 :g 0 :h 0 :f 0 :parent nil}))))
